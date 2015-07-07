a316f2fbe8263201cb1bf101943cc2d02fa88de5
Gary Joy <Gary@Garys-MacBook-Air.local>
2015-07-07 19:41:42 +1000

    Initial Commit

---
+++ Scales/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Scales
+//
+//  Created by Gary Joy on 07/07/2015.
+//  Copyright (c) 2015 Enjoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
